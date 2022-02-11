2dd5adf512c7ba8df30bf2a28ce01903b4ca1d7d
Adnane-Louardi-dev <adnanelouardi.dev@gmail.com>
2022-02-11 20:55:58 +0100

    fix player bugs

--- components/themes-list.js
+++ components/themes-list.js
@@ -46,7 +46,7 @@ const ThemesList = ({ data }) => {
                     }
                     e.currentTarget.classList.add("opacity-100");
                   }}
-                  className=" opacity-0 flex flex-col absolute t-0 b-0 h-48 w-40 z-20 justify-center items-center backdrop-brightness-50 rounded-3xl"
+                  className="opacity-0 group-hover:opacity-100 flex flex-col absolute t-0 b-0 h-48 w-40 z-20 justify-center items-center backdrop-brightness-50 rounded-3xl"
                 >
                   {PlayerOn ? (
                     <svg
