c424e59d387679764152200c32e5f0b5b6caa3eb
Adnane-Louardi-dev <adnanelouardi.dev@gmail.com>
2022-02-12 06:49:10 +0100

    add anime opening, anime other names features

--- pages/anime/[anime].js
+++ pages/anime/[anime].js
@@ -2,7 +2,7 @@ import Image from "next/image";
 import Link from "next/link";
 import Head from "next/head";
 import { useRouter } from "next/router";
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useRef } from "react";
 import axios from "axios";
 import ThemeAnime from "../../components/theme-anime";
 import AnimePlaceholder from "../../components/anime-placeholder";
@@ -14,8 +14,10 @@ export default function Anime() {
   const { anime } = router.query;
   const [loading, setLoading] = useState(false);
   const [data, setData] = useState();
+  // const [Mirrors, setMirrors] = useState([]);
+  // const [ExactMirror, setExactMirror] = useState("");
+  // const VidPlayer = useRef();
 
-  console.log(data);
   useEffect(() => {
     setLoading(!loading);
     axios
@@ -31,8 +33,12 @@ export default function Anime() {
       })
 
       .catch((err) => console.log(err));
+    // data?.themes.forEach((theme) => {
+    //   Mirrors.push({ type: theme.type, mirror: theme.mirrors[0].mirror });
+    // });
     return () => setData();
   }, [anime]);
+
   return (
     <>
       <TopBar />
@@ -59,39 +65,46 @@ export default function Anime() {
               </div>
               <div className="flex flex-col w-1/2 justify-between">
                 <div className="mt-5 mx-4">
-                  <h3 className="font-oxygen text-gray-400 dark:text-gray-300 text-xs">{`#${data.mal_id}`}</h3>
-                  <h1 className="font-openSans text-gray-700 dark:text-white text-xl w-full tracking-wide whitespace-nowrap truncate">
+                  <h3 className="font-oxygen my-2 text-gray-400 dark:text-gray-300 text-xs">{`#${data.mal_id}`}</h3>
+                  <h1 className="font-openSans my-2 text-gray-700 dark:text-white text-xl w-full tracking-wide whitespace-nowrap truncate">
                     {data.title.slice(-1)[0]}
                   </h1>
-                  <div className="flex my-2">
+                  <div className="flex my-4">
                     <p className="font-oxygen mr-1 py-1 px-2 text-white bg-yellow-500 text-xs rounded-xl whitespace-nowrap shadow-md">{data.season}</p>
                     <p className="font-oxygen mr-1 py-1 px-2 text-white bg-yellow-500 text-xs rounded-xl whitespace-nowrap shadow-md">
                       {`${data.themes.length} theme(s)`}
                     </p>
                   </div>
+                  <div className="font-openSans my-2 text-gray-700 dark:text-white text-xl w-full tracking-wide whitespace-nowrap truncate">
+                    <h1 className="font-oxygen my-2 text-gray-400 dark:text-gray-300 text-sm">Other Names:</h1>
+                    {data.title.map((el, i) => (
+                      <p key={i} className="font-lato dark:text-gray-400 text-gray-300 text-xs ">
+                        {el},
+                      </p>
+                    ))}
+                  </div>
                 </div>
-
-                <button className="w-full md:-1/2 flex items-center text-center text-gray-700 focus:text-white w-full mx-2 mb-5 px-2 py-1 bg-white focus:bg-violet-500 ring-2 ring-gray-700 rounded-xl shadow-lg">
-                  <svg xmlns="http://www.w3.org/2000/svg" className="h-9 w-9" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                    <path
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      strokeWidth={2}
-                      d="M14.752 11.168l-3.197-2.132A1 1 0 0010 9.87v4.263a1 1 0 001.555.832l3.197-2.132a1 1 0 000-1.664z"
-                    />
-                    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
-                  </svg>
-                  <Link href={data.themes[0].mirrors[0].mirror} passHref>
-                    <h1 className="font-lato my-2 mx-1 text-md whitespace-nowrap truncate">watch anime opening</h1>
-                  </Link>
-                </button>
               </div>
             </div>
+
             <div className="mx-3 mb-5 rounded-xl overflow-hidden">
               <Player playsInline poster={data.cover} src={data.themes[0].mirrors[0].mirror}>
                 <BigPlayButton position="center" />
               </Player>
             </div>
+          </div>
+          <div className="grid justify-items-end ">
+            <button className="flex items-center justify-center text-center text-gray-400 focus:text-gray-300 mx-2 px-2 py-1">
+              <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
+                <path
+                  fillRule="evenodd"
+                  d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
+                  clipRule="evenodd"
+                />
+              </svg>
+
+              <h1 className="font-lato my-2  text-sm whitespace-nowrap truncate">{`${data.themes.length} Songs available`}</h1>
+            </button>
           </div>
 
           <ThemeAnime props={data} />
@@ -101,4 +114,33 @@ export default function Anime() {
       )}
     </>
   );
+}
+{
+  /* <form id="form1">
+                    <select width="300" className="font-lato my-2 mx-1 text-md whitespace-nowrap truncate">
+                      <>
+                        {Mirrors.length > 0
+                          ? Mirrors.map((item, i) => {
+                              const { type, mirror } = item;
+                              return (
+                                <option
+                                  onClick={() => {
+                                    VidPlayer.current.load();
+                                    setExactMirror(mirror);
+                                    console.log(mirror);
+                                  }}
+                                  value={type}
+                                  key={i}
+                                >
+                                  {type}
+                                </option>
+                              );
+                            })
+                          : null}
+                      </>
+                      <option value="Select trailer" defaultValue>
+                        Select trailer
+                      </option>
+                    </select>
+                  </form> */
 }
